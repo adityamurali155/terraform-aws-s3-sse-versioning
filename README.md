6df690042578dfd3cb19e2ac95324bf86c99418f
adityamurali155 <adityamurali2@gmail.com>
2024-02-09 16:57:49 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a module that provisions an S3 bucket with versioning and Server Side Encryption (SSE) enabled.
+
+For usage, you can directly import this module and run the terraform workflow (init, plan, apply).
---
+++ main.tf
@@ -0,0 +1,17 @@
+module "s3-bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "4.1.0"
+  bucket = "env0"
+  
+  versioning = {
+    enabled = true
+  }
+
+  server_side_encryption_configuration = {
+    rule = {
+      apply_server_side_encryption_by_default = {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
